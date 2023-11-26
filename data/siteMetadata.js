2005bc8ebaedbadf6b2f3cd7bb6a370407863973
Tanaka Kent <tanaken0304.0123@gmail.com>
2023-11-26 22:20:54 +0900

    update site meta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'ひつじ小屋の日常',
+  author: 'k3nt',
+  headerTitle: 'ひつじ小屋の日常',
+  description: '新卒2年目MLエンジニアの日常を呟きます。',
+  language: 'jp',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteRepo: '',
+  siteLogo: '/static/images/kent.jpg',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/kent0304',
+  twitter: 'https://twitter.com/k3ntrr',
+  linkedin: 'https://www.linkedin.com/in/kento-tanaka/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
